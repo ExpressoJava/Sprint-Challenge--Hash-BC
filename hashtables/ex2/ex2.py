9a1e8f97dd35f9e45b1db0a3801263e783d4e5c8
Bao Pham <ryanvuepham@gmail.com>
2019-10-12 12:23:55 -0500

    finish hastables, ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -13,11 +13,24 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
+    # Generate a hash equal to the length of the trip
     hashtable = HashTable(length)
+    # Generate an array to store the trip in order
     route = [None] * length
 
     """
     YOUR CODE HERE
     """
+    # Insert each ticket into the hastable: key="source", value="destination"
+    for i in range(length):
+        hash_table_insert(hashtable, tickets[i].source, tickets[i].destination)
+
+    # Find the "NONE" source key and make it the first item in route array
+    route[0] = hash_table_retrieve(hashtable, 'NONE')
+
+    # Map the values in the hash table to the route in order according to hint in README
+    for i in range(1, length):
+        route[i] = hash_table_retrieve(hashtable, route[i-1])
+
 
     return route
